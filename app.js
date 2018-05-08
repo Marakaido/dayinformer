d7dc3c5b80866b38037e2567a27e6f4f7f3e573a
Marakaido <yaroslav.mykoliv28@gmail.com>
2018-05-08 18:39:41 +0300

    Add api query

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var url = 'http://graph.facebook.com/517267866/?fields=picture';
+
+app.get('/', (req, response) => {
+  http.get(url, function(res){
+      var body = '';
+
+      res.on('data', function(chunk){
+          body += chunk;
+      });
+
+      res.on('end', function(){
+          var fbResponse = JSON.parse(body);
+          console.log("Got a response: ", fbResponse);
+          response.send(fbResponse)
+      });
+  }).on('error', function(e){
+        console.log("Got an error: ", e);
+  });
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
