bb63758576387d5e5da2ebf3ca32e4059d787e32
Marakaido <yaroslav.mykoliv28@gmail.com>
2018-05-10 22:42:51 +0300

    Add style

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ function launchesTemplate(data) {
     if (data.length != 0) {
         let result = launchTemplate(data[0]);
         for (let i = 1; i < data.length; i++)
-            result += '<hr />' + launchTemplate(data[i]);
+            result += launchTemplate(data[i]);
             
         return result;
     }
@@ -22,35 +22,41 @@ function launchesTemplate(data) {
 }
 
 function launchTemplate(data) {
-    return `
-            <table>
-                <tr><td>Name:</td><td>${data.name}</td></tr>
-                <tr><td>Starts:</td><td>${data.start}</td></tr>
-                <tr><td>Ends:</td><td>${data.end}</td></tr>
-                <tr><td>Info:</td><td>${linkArrayTemplate(data.info)}</td></tr>
-                <tr><td>Videos:</td><td>${linkArrayTemplate(data.vids)}</td></tr>
-                <tr><td>Location:</td><td>${data.location}</td></tr>
+    return `<div class="card">
+            <table cellspacing="0" cellpadding="0">
+                <colgroup>
+                    <col class="fieldSpecifier" />
+                    <col />
+                </colgroup>
+                <tbody>
+                    <tr><td class="label">Name:</td><td style="font-weight: 700;">${data.name}</td></tr>
+                    <tr><td class="label">Starts:</td><td>${data.start}</td></tr>
+                    <tr><td class="label">Ends:</td><td>${data.end}</td></tr>
+                    <tr><td class="label">Info:</td><td>${linkArrayTemplate(data.info)}</td></tr>
+                    <tr><td class="label">Videos:</td><td>${linkArrayTemplate(data.vids)}</td></tr>
+                    <tr><td class="label">Location:</td><td>${data.location}</td></tr>
+                </tbody>
             </table>
+            </div>
     `;
 }
 
 function linkArrayTemplate(data) {
     if(data == null || data.length == 0) return 'no data';
 
-    let result = '<ul>';
-    data.forEach((el) => {
-        result += `<li><a href="${el}">${el}</a></li>`;
-    });
-    result += '</ul>';
+    let result = `<a href="${data[0]}">${data[0]}</a>`;
+    for(let i = 1; i < data.length; i++)
+        result += `<br /><a href="${el}">${el}</a>`;
+
     return result;
 }
 
 function issTemplate(data) {
     return `
-        <h1>International Space Station</h1>
-        <table>
-        <tr><td><p>Location: </p></td><td>${locationTemplate(data.location)}</td></tr>
-        <tr><td valign="top"><p>Astronauts: </p></td><td>${peopleTemplate(data.people)}</td></tr>
+        <div class="asideHeader">International Space Station</div>
+        <table style="margin: auto;">
+        <tr><td><p style="font-weight: 700;">Location: </p></td><td>${locationTemplate(data.location)}</td></tr>
+        <tr><td valign="top"><p style="font-weight: 700;">Astronauts: </p></td><td>${peopleTemplate(data.people)}</td></tr>
         </table>
     `;
 }
@@ -74,7 +80,7 @@ function locationTemplate(data) {
 
 function factTemplate(data) {
     return `
-        <h1>Fun fact of the day</h1>
-        <p>${data}</p>
+        <div class="asideHeader">Fun fact of the day</div>
+        <p style="margin: 10px; text-align: justify;">${data}</p>
     `;
 }
